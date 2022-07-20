2e2b5a7f663d3707c5c0a53b7d888b9d422ea782
Oliver Marcell Toth <oliverm.toth@pm.me>
2022-07-20 20:42:01 +0200

    update

---
+++ algoexpert/easy/productSum.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+#include <any>
+using namespace std;
+typedef long long ll;
+
+
+int productSum(vector<any> v){
+	in
+}
