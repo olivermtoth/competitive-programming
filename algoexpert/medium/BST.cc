07e4d74e3fe88300e2e8872413f84c7cb3950e68
Oliver Marcell Toth <oliverm.toth@pm.me>
2022-07-22 04:05:45 +0200

    2022.07.22 practice.

--- algoexpert/medium/BST.cc
+++ algoexpert/medium/BST.cc
@@ -28,6 +28,93 @@ struct BST{
 		right = NULL;
 	}
 
-	
+	BST &insert(int value){
+		if(this->value > value){
+			if(left == NULL){
+				BST *newBST = new BST(value);
+				left = newBST;
+			}
+			else{
+				left->insert(value);
+			}
+		}
+		else{
+			if(right == NULL){
+				BST *newBST = new BST(value);
+				right = newBST;
+			}
+			right->insert(value);
+		}
+		return *this;
+	}
+
+	bool contains(int value){
+		if(this->value > value){
+			if(left == NULL){
+				return false;
+			}
+			else{
+				left->contains(value);
+			}
+		}
+		else if(this->value < value){
+			if(right == NULL){
+				return false;
+			}
+			else{
+				right->contains(value);
+			}
+		}
+		return true;
+
+	}
+
+	BST &remove(int val, BST *parent=NULL){
+		if(val < value){
+			if(left != NULL){
+				left->remove(value, this);
+			}
+		}
+		else if(val > value){
+			if(right != NULL){
+				right->remove(value, this);
+			}
+		}
+		else{
+			if(left != NULL && right != NULL){
+				value = right->getMinValue();
+				right->remove(value, this);
+			}
+			else if(parent == NULL){
+				if(left != NULL){
+					value = left->value;
+					right = left->right;
+					left = left->left;
+				}
+				else if(right != NULL){
+					value = right->value;
+					right = right->right;
+					left = left->left;
+				}
+			}
+			else if(parent->left == this){
+				parent->left = left != NULL ? left : right;
+			}
+			else if(parent->right == this){
+				parent->right = right != NULL? left : right;
+			}
+		}
+		return *this;
+	}
+
+	// time:O(log(n)); space:O(1)
+	int getMinValue(){
+		if(left == NULL){
+			return value;
+		}
+		else{
+			return left->getMinValue();
+		}
+	}
 	
 };
