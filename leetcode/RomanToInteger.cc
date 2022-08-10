72c99672b299c1038343ddac51478246f18998c9
Oliver Marcell Toth <olivermtoth@protonmail.ch>
2022-08-10 16:25:16 +0200

    Update.

--- leetcode/RomanToInteger.cc
+++ leetcode/RomanToInteger.cc
@@ -20,10 +20,10 @@ typedef long long ll;
 
 int main(){
 	string s;
-	int res = 0
+	int res = 0, p = 0;
 	unordered_map<char, int> m = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-	for(int i = 1; i<s.lenght(); i++){
-		if(
+	for(int i = s.size()-1; i >= 0; i--){
+		res += m[s[i]];
 	}
 	return 0;
 }
